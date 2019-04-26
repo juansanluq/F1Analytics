10a3901cd9c8452ad7dfb6b7250df5660f338824
juansanluq <juansanluq@gmail.com>
2019-04-26 14:48:02 +0200

    Minor changes, prleoading activated, styles changes to circuits page

--- f1-analytics/src/app/app-routing.module.ts
+++ f1-analytics/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { CircuitsIndexComponent } from './components/circuits-index/circuits-index.component';
 import { AppComponent } from './app.component';
@@ -10,7 +10,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
